1d2baa8ea141301793a9985bdc179537c62041f3
Max Heinze <maxheinze@googlemail.com>
2026-05-24 11:30:45 +0200

    chore: strip Top Down template, reinit as blank "Onset" project

--- Project/Source/MPTDARPG/MPTDARPG.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MPTDARPG.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MPTDARPG, "MPTDARPG" );
-
-DEFINE_LOG_CATEGORY(LogMPTDARPG)
- 
--- Project/Source/MPTDARPG/MPTDARPGGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MPTDARPGGameMode.h"
-
-AMPTDARPGGameMode::AMPTDARPGGameMode()
-{
-	// stub
-}
